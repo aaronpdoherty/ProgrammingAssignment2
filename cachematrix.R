d823eeb05c4e991e2e6fb67855ea52985c41d939
Aaron P Doherty <doherty.ap@gmail.com>
2015-05-24 12:20:07 -0400

    Programming Assignment 2
    code by Aaron Patrick Doherty, based on code provided in the assignment
    description and the MASS package discovered via R forums

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function tests if the MASS package is installed as it 
+## makes use of the ginv() function. If TRUE, the function
+## caches the inverse of a matrix, returning a list containing a function
+## to store the inverse of a matrix in a cached symbol
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- <- function(mx = matrix()) {
+  
+  MASStest <<- is.element("MASS", installed.packages()[,1])
+  
+  if(MASStest == TRUE) {
+    m <- NULL
+    set <- function(y) {
+      mx <<- y
+      m <<- NULL
+    }
+    get <- function() mx
+    setinv <- function(ginv) m <<- ginv
+    getinv <- function() mx
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  }
+  else {
+    print("Please use install.packages() to install the MASS package")
+  }
 }
 
 
-## Write a short comment describing this function
+## This function tests if the MASS package is installed as it 
+## makes use of the ginv() function. If TRUE, the function
+## searches for the inverse of a matrix in the global environment
+## comupting the result if it is not found
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- <- function(x, ...) {
+  MASStest <<- is.element("MASS", installed.packages()[,1])
+  if(MASStest == TRUE) {
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- ginv(data, ...)
+    x$setinv(m)
+    m
+  }
+  else {
+    print("Please use install.packages() to install the MASS package")
+  }
 }
